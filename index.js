6ced0aa9d15b75f4552961311dfee426b509fe20
Satil Pereira <satil.prog@gmail.com>
2022-11-16 16:59:32 -0300

    callback studying

---
+++ index.js
@@ -0,0 +1,21 @@
+const rectangle = require('./rectangle');
+let rect = require('./rectangle');
+
+function solveRect(l,b) {
+    console.log(`Solving for rectangle with l = ${l} and b = ${b}`);
+    
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log(`Error: ${err.message}`);
+        } else {
+            console.log(`The area of the rectangle of dimensions l = ${l}, b = ${b} is ${rectangle.area()}`);
+            console.log(`The perimeter of the rectangle of dimensions l = ${l}, b = ${b} is ${rectangle.perimeter()}`);
+        }
+    });
+}
+
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
